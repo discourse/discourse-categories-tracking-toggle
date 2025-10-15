430e188da1cb38b4bb65f9d7a6132984350d9cfc
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2025-10-15 16:39:20 +0800

    DEV: Fix incorrectly written tests (#49)

--- spec/system/follow_category_spec.rb
+++ spec/system/follow_category_spec.rb
@@ -6,36 +6,36 @@ RSpec.describe "Category Tracking Toggle", system: true do
 
   let!(:theme) { upload_theme_component }
 
-  before do
-    sign_in(user)
-    CategoryUser.set_notification_level_for_category(
-      user,
-      NotificationLevels.all[:regular],
-      category.id,
-    )
-  end
+  before { sign_in(user) }
 
   context "when visiting the categories page" do
-    before { visit "/categories" }
-
-    it "displays the category toggle button" do
-      expect(page).to have_css("[data-category-id='#{category.id}'] .custom-category-toggle")
-    end
-
     it "mutes the category when clicking the toggle" do
+      CategoryUser.set_notification_level_for_category(
+        user,
+        NotificationLevels.all[:regular],
+        category.id,
+      )
+
+      visit "/categories"
+
       find("[data-category-id='#{category.id}'] .custom-category-toggle").click
 
-      try_until_success do
-        expect(CategoryUser.lookup(user, :muted).pluck(:category_id)).to include(category.id)
-      end
+      expect(CategoryUser.lookup(user, :muted).pluck(:category_id)).to include(category.id)
     end
 
     it "unmutes the category when clicking the toggle again" do
+      CategoryUser.set_notification_level_for_category(
+        user,
+        NotificationLevels.all[:muted],
+        category.id,
+      )
+
+      visit "/categories"
+
+      find(".muted-categories").click
       find("[data-category-id='#{category.id}'] .custom-category-toggle").click
 
-      try_until_success do
-        expect(CategoryUser.lookup(user, :regular).pluck(:category_id)).to include(category.id)
-      end
+      expect(CategoryUser.lookup(user, :regular).pluck(:category_id)).to include(category.id)
     end
   end
 end
